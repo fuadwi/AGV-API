e2dbad0322cee7f63d9528637bc9ef7c676a84dc
Rachmat Riyanto <raryant@outlook.com>
2019-09-23 09:42:43 +0700

    hampir jadi mase

--- Dispatching-API/Form1.Designer.cs
+++ Dispatching-API/Form1.Designer.cs
@@ -29,12 +29,158 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.modbusCom = new System.IO.Ports.SerialPort(this.components);
+            this.modbusWorker = new System.ComponentModel.BackgroundWorker();
+            this.btnStartService = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStopService = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.apiWorker = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Dispatching API Service Started!";
+            this.notifyIcon1.BalloonTipTitle = "Infiniti 4.0";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_MouseDoubleClick);
+            // 
+            // modbusWorker
+            // 
+            this.modbusWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.ModbusWorker_DoWork);
+            // 
+            // btnStartService
+            // 
+            this.btnStartService.Location = new System.Drawing.Point(6, 19);
+            this.btnStartService.Name = "btnStartService";
+            this.btnStartService.Size = new System.Drawing.Size(75, 23);
+            this.btnStartService.TabIndex = 0;
+            this.btnStartService.Text = "Start";
+            this.btnStartService.UseVisualStyleBackColor = true;
+            this.btnStartService.Click += new System.EventHandler(this.BtnStartService_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblStatus);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnStopService);
+            this.groupBox1.Controls.Add(this.btnStartService);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(170, 107);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Service Controller";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BackColor = System.Drawing.Color.Red;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(6, 64);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(156, 33);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "STOP";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Service Status";
+            // 
+            // btnStopService
+            // 
+            this.btnStopService.Enabled = false;
+            this.btnStopService.Location = new System.Drawing.Point(87, 19);
+            this.btnStopService.Name = "btnStopService";
+            this.btnStopService.Size = new System.Drawing.Size(75, 23);
+            this.btnStopService.TabIndex = 1;
+            this.btnStopService.Text = "Stop";
+            this.btnStopService.UseVisualStyleBackColor = true;
+            this.btnStopService.Click += new System.EventHandler(this.BtnStopService_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "CONFIG";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox2);
+            this.groupBox3.Location = new System.Drawing.Point(188, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(260, 162);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "API REQUEST LOG";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(6, 16);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(247, 134);
+            this.listBox2.TabIndex = 3;
+            // 
+            // apiWorker
+            // 
+            this.apiWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.ApiWorker_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(456, 186);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AGV DISPATCHING API";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.IO.Ports.SerialPort modbusCom;
+        private System.ComponentModel.BackgroundWorker modbusWorker;
+        private System.Windows.Forms.Button btnStartService;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStopService;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.ComponentModel.BackgroundWorker apiWorker;
     }
 }
 
--- Dispatching-API/Form1.cs
+++ Dispatching-API/Form1.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Code4Bugs.Utils.IO;
+using Code4Bugs.Utils.IO.Modbus;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Modbus.Device;
+using System.Net;
+using System.IO;
 
 namespace Dispatching_API
 {
@@ -15,6 +21,190 @@ namespace Dispatching_API
         public Form1()
         {
             InitializeComponent();
+        }
+        private Modbus.Data.DataStore dataStore;
+        private static HttpListener listener;
+        private static string url;
+
+        [Obsolete]
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            url = System.Configuration.ConfigurationSettings.AppSettings["listenHost"] + ":" + System.Configuration.ConfigurationSettings.AppSettings["listenPort"] + "/";
+            Console.WriteLine("URL = {0}", url);
+            dataStore = Modbus.Data.DataStoreFactory.CreateDefaultDataStore();
+            dataStore.HoldingRegisters[102] = 22;
+        }
+
+
+        public static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            foreach (byte b in ba)
+                hex.AppendFormat("{0:x2}", b);
+            return hex.ToString();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NotifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        [Obsolete]
+        private void BtnStartService_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                modbusCom.PortName = System.Configuration.ConfigurationSettings.AppSettings["comPort"];
+                modbusCom.BaudRate = Convert.ToInt32(System.Configuration.ConfigurationSettings.AppSettings["comBaud"]);
+                modbusCom.DataBits = 8;
+                modbusCom.Parity = Parity.None;
+                modbusCom.StopBits = StopBits.One;
+                modbusCom.Open();
+                modbusWorker.RunWorkerAsync();
+                btnStartService.Enabled = false;
+                btnStopService.Enabled = true;
+                lblStatus.Text = "RUNNING";
+                lblStatus.BackColor = Color.Green;
+                apiWorker.RunWorkerAsync();
+
+                
+            }
+            catch
+            {
+                if(modbusCom.IsOpen) modbusCom.Close();
+            }
+        }
+
+        private void ModbusWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ModbusSerialSlave ms = ModbusSerialSlave.CreateRtu(1, modbusCom);
+            ms.DataStore = dataStore;
+            ms.Listen();
+        }
+
+        private void BtnStopService_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!modbusWorker.IsBusy) modbusWorker.CancelAsync();
+                if (!apiWorker.IsBusy) apiWorker.CancelAsync();
+                modbusCom.Close();
+                btnStartService.Enabled = true;
+                btnStopService.Enabled = false;
+                lblStatus.Text = "STOP";
+                lblStatus.BackColor = Color.Red;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public static async Task HandleIncomingConnections()
+        {
+            bool runServer = true;
+
+            // While a user hasn't visited the `shutdown` url, keep on handling requests
+            while (runServer)
+            {
+                // Will wait here until we hear from a connection
+                HttpListenerContext ctx = await listener.GetContextAsync();
+
+                // Peel out the requests and response objects
+                HttpListenerRequest req = ctx.Request;
+                HttpListenerResponse resp = ctx.Response;
+                //CORS - KIRIM DATA REPLY LANGSUNG KE WMS
+                if (req.HttpMethod == "OPTIONS")
+                {
+                    resp.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+                    resp.AddHeader("Access-Control-Allow-Methods", "GET, POST");
+                    resp.AddHeader("Access-Control-Max-Age", "1728000");
+                }
+                resp.AppendHeader("Access-Control-Allow-Origin", "*");
+
+                // Make sure we don't increment the page views counter if `favicon.ico` is requested
+                if (req.Url.AbsolutePath != "/favicon.ico")
+                {
+                    Console.WriteLine(req.Url.ToString());
+                    Console.WriteLine(req.HttpMethod);
+                    Console.WriteLine(req.UserHostName);
+                    Console.WriteLine("Request to route " + req.QueryString["route"]);
+                    Console.WriteLine();
+                }
+
+                // Write the response info
+                string disableSubmit = !runServer ? "disabled" : "";
+                byte[] data;
+                //data = Encoding.UTF8.GetBytes("Casun AGV API");
+                //Console.WriteLine(req.QueryString["route"]);
+                if (String.IsNullOrEmpty(req.QueryString["route"]))
+                {
+                    data = Encoding.UTF8.GetBytes("Casun AGV API");
+                }
+                else
+                {
+                    try
+                    {
+                        int route = Convert.ToInt32(req.QueryString["route"]);
+                        
+                        data = Encoding.UTF8.GetBytes("Accepted, route=" + route.ToString());
+                    }
+                    catch
+                    {
+                        data = Encoding.UTF8.GetBytes("Wrong data type");
+                    }
+                }
+                resp.ContentType = "text/html";
+                resp.ContentEncoding = Encoding.UTF8;
+                resp.ContentLength64 = data.LongLength;
+
+                // Write out to the response stream (asynchronously), then close it
+                await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                resp.Close();
+            }
+        }
+        private static byte[] IconToBytes(Icon icon)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                icon.Save(ms);
+                return ms.ToArray();
+            }
+        }
+
+        private static Icon BytesToIcon(byte[] bytes)
+        {
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                return new Icon(ms);
+            }
+        }
+        private void ApiWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                // Create a Http server and start listening for incoming connections
+                listener = new HttpListener();
+                listener.Prefixes.Add(url);
+                listener.Start();
+                Console.WriteLine("Listening for connections on {0}", url);
+
+                // Handle requests
+                Task listenTask = HandleIncomingConnections();
+                listenTask.GetAwaiter().GetResult();
+
+                // Close the listener
+                listener.Close();
+            }
         }
     }
 }
